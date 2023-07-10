0f52ced9b99ad8ba77ae35ff02c05d75d195b773
ĽUBOSLAV <lubiku@proton.me>
2023-07-10 22:03:16 +0200

    modified structure of project container and implemented a behavior of container element basend on screen mode

--- scripts/root.js
+++ scripts/root.js
@@ -7,7 +7,7 @@ let modeSwitcherButton = window.document.getElementById("mode-changer");
 window.document.addEventListener('DOMContentLoaded', function() {
     let userStoredMode = localStorage.getItem('mode');
     let bodyEle = document.body;
-    
+
     // Check the current mode
     if (userStoredMode === 'dark') {
         // Switch for dark mode
@@ -37,13 +37,33 @@ modeSwitcherButton.addEventListener('click', function changeMode() {
         bodyEle.classList.add('dark-mode');
         localStorage.setItem('mode', 'dark');
         modeSwitcherButton.innerText = "GO LIGHT";
+        switchProjectContainersMode('dark');
+
     } else {
         // Switch for light mode
         bodyEle.classList.remove('dark-mode');
         bodyEle.classList.add('light-mode');
         localStorage.setItem('mode', 'light');
         modeSwitcherButton.innerText = "GO DARK";
+        switchProjectContainersMode('light');
     }
 })
+
+function switchProjectContainersMode(mode) {
+    let projectContainers = window.document.getElementsByClassName("project-container");
+    if (mode === 'light') {
+        for (let projectContainer of projectContainers) {
+            // Switch project containers
+            projectContainer.classList.remove('light-container');
+            projectContainer.classList.add('dark-container');
+        }
+    } else if (mode === 'dark') {
+        for (let projectContainer of projectContainers) {
+            // Switch project containers
+            projectContainer.classList.remove('dark-container');
+            projectContainer.classList.add('light-container');
+        }
+    }
+}
 
 
